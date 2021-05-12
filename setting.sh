bd14e71cd290b570a0349978968e96636b04977c
SeitaHigashi <higashi110902@gmail.com>
2021-05-12 13:51:17 +0900

    Add setting.sh

---
+++ setting.sh
@@ -0,0 +1,12 @@
+# Install dein.vim
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ./installer.sh
+sh ./install.sh .local/share/nvim
+rm ./install.sh
+
+# Install nodejs
+# Using ubuntu
+# Using Ubuntu
+# curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+
+
