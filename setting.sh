8d07b7691c0cbaa86806ac6a2670ccbf817a2310
Sepi <higashi110902@gmail.com>
2021-05-12 15:56:29 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -1,12 +1,21 @@
 # Install dein.vim
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/.local/share/nvim
-rm ./installer.sh
+if [ -e ~/.local/share/nvim/dein ]; then
+  echo "dein.vim is already installed."
+else
+  echo "Install dein.vim to ~/.local/share/nvim."
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+  sh ./installer.sh ~/.local/share/nvim
+  rm ./installer.sh
+fi
 
 # Install nodejs
-# Using ubuntu
-# Using Ubuntu
-# curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-# sudo apt-get install -y nodejs
+if type "node" > /dev/null 2>&1; then
+  echo "nodejs is already installed."
+else
+  echo "nodejs not found."
+  # curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+  # sudo apt-get install -y nodejs
+fi
+
 
 
