bc12edf3d9c7cb5a458668ce378175e46f9f81a6
SeitaHigashi <higashi110902@gmail.com>
2021-12-20 23:12:58 +0900

    Update lsp config

--- nvim/plugins/nvim-lspconfig.rc.lua
+++ nvim/plugins/nvim-lspconfig.rc.lua
@@ -33,11 +33,11 @@ local on_attach = function(client, bufnr)
 
 end
 
-local lsp_installer = require("nvim-lsp-installer")
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
-    opts.on_attach = on_attach
-
-    server:setup(opts)
-    vim.cmd [[ do User LspAttachBuffers ]]
-end)
+-- local lsp_installer = require("nvim-lsp-installer")
+-- lsp_installer.on_server_ready(function(server)
+--     local opts = {}
+--     opts.on_attach = on_attach
+-- 
+--     server:setup(opts)
+--     vim.cmd [[ do User LspAttachBuffers ]]
+-- end)
