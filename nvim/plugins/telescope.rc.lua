ae660aa18dcb00600d32605d99b74c8076e67065
SeitaHigashi <higashi110902@gmail.com>
2021-12-23 14:47:21 +0900

    Setting Keymap for 'telescope.nvim'.

--- nvim/plugins/telescope.rc.lua
+++ nvim/plugins/telescope.rc.lua
@@ -1,3 +1,24 @@
 vim.api.nvim_set_keymap('n', '<Leader>j', [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>k', [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>d', [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
+
+local actions = require('telescope.actions')
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["J"] = actions.move_selection_next,
+        ["K"] = actions.move_selection_previous,
+        ["Q"] = actions.close,
+        ["<esc>"] = actions.close,
+        ["L"] = actions.file_edit,
+        ["E"] = actions.file_vsplit,
+        ["H"] = actions.file_split,
+      },
+      n = {
+        ["q"] = actions.close,
+        ["Q"] = actions.close,
+      }
+    }
+  }
+}
