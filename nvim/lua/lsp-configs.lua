c2cab1c08df01b2d473ec3e048f08849d96df8bc
SeitaHigashi <higashi110902@gmail.com>
2022-01-05 19:55:27 +0900

    Update Diagnostic config

--- nvim/lua/lsp-configs.lua
+++ nvim/lua/lsp-configs.lua
@@ -1,3 +1,4 @@
+-- Diagnostic
 local signs = {
   Error = " ",
   Warn = " ",
@@ -9,3 +10,13 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  sererity_sort = false,
+})
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
