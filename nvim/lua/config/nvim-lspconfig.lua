7763615be691b65d05d7104dc18fd2374259db83
SeitaHigashi <higashi110902@gmail.com>
2022-01-05 19:44:15 +0900

    Update keybinds

--- nvim/lua/config/nvim-lspconfig.lua
+++ nvim/lua/config/nvim-lspconfig.lua
@@ -25,7 +25,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<Leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap('n', '<Leader>ft', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap('n', '<Leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
 end
 
