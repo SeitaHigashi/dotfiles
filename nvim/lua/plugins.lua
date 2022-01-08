331a5ceeabc3dcce9391b0ce2d4641a1c6a29c89
SeitaHigashi <higashi110902@gmail.com>
2022-01-08 14:28:05 +0900

    Add nvim-cmp-lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -29,6 +29,7 @@ require('packer').startup(function()
     requires = {
       { 'neovim/nvim-lspconfig' },
       { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
       { 'hrsh7th/cmp-buffer' },
       { 'hrsh7th/cmp-path' },
       { 'hrsh7th/cmp-cmdline' },
