7b983bb1001052dac631d1535d81c359c2ad0f82
SeitaHigashi <higashi110902@gmail.com>
2022-06-11 00:23:43 +0900

    Fix nvim-comp for breaking changes

--- nvim/lua/config/nvim-cmp.lua
+++ nvim/lua/config/nvim-cmp.lua
@@ -81,8 +81,10 @@ cmp.setup({
       return vim_item
     end,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  window = {
+    documentation = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    }
   },
   experimental = {
     ghost_text = true,
