bb3eb383c9a75ef23fab7ac65fb5b5f01eae7dea
SeitaHigashi <higashi110902@gmail.com>
2022-08-14 17:40:26 +0900

    Added the lsp configuration

--- nvim/lua/config/mason-lspconfig.lua
+++ nvim/lua/config/mason-lspconfig.lua
@@ -5,7 +5,16 @@ mason_lspconfig.setup_handlers {
   function(server_name)
     local opts = {}
     opts.on_attach = require('lsp-configs').on_attach
+
+    local status, config = pcall(function ()
+      return require("lsp-config." .. server_name)
+    end)
+    if status == true then
+      opts.settings = config
+    end
+
     nvim_lsp[server_name].setup(opts)
     vim.cmd [[ do User LspAttachBuffers ]]
   end
 }
+
