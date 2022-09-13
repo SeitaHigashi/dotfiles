f1b6d0abd66ebc964a2aa8565a3f722e2618985f
SeitaHigashi <higashi110902@gmail.com>
2022-09-14 00:16:03 +0900

    Update the nvim-cmp config
    Unuse to table.unpack function

--- nvim/lua/config/nvim-cmp.lua
+++ nvim/lua/config/nvim-cmp.lua
@@ -7,7 +7,9 @@ lspkind.init {
 }
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local cursor_table = vim.api.nvim_win_get_cursor(0)
+  local line = cursor_table[1]
+  local col = cursor_table[2]
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
