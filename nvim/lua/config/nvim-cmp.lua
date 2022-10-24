734742f61ed66b216fae413f899093ef5b9aacd3
SeitaHigashi <higashi110902@gmail.com>
2022-10-24 20:43:15 +0900

    Update the code action lightbulb config

--- nvim/lua/config/nvim-cmp.lua
+++ nvim/lua/config/nvim-cmp.lua
@@ -13,7 +13,7 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-    cmp.setup({
+cmp.setup({
   completion = {
     completeopt = "menu,menuone,noselect",
   },
