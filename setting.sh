36ccc9cc931f18ae7df7979c85c74b04178e3c68
SeitaHigashi <higashi110902@gmail.com>
2023-01-01 00:46:54 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -1,28 +1,51 @@
-# Install dein.vim
-if [ -e ~/.local/share/nvim/dein ]; then
-  echo "dein.vim is already installed."
+#!/bin/bash
+
+# Setting the text format.
+installed="\e[32minstalled\e[m"
+not_found="\e[31mnot found\e[m"
+
+# Install neovim
+if type "nvim" > /dev/null 2>&1; then
+  echo -e "neovim is already $installed."
 else
-  echo "Install dein.vim to ~/.local/share/nvim."
-  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-  sh ./installer.sh ~/.local/share/nvim
-  rm ./installer.sh
+  echo -e "neovim is $not_found."
+  echo -e "Please install neovim."
+fi
+
+# Install git
+if type "git" > /dev/null 2>&1; then
+  echo -e "git is already $installed."
+else
+  echo -e "git is $not_found."
+  echo -e "Please install git."
+  exit 1
+fi
+
+packer_dir="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+
+echo -e "checking packer that is plugin manager for neovim..."
+if [ ! -d $packer_dir ]; then
+  echo -e "packer is $not_found."
+  echo -e "try to install packer.nvim"
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim $packer_dir
+else
+  echo -e "packer is already $installed."
 fi
 
 # Install ripgrep
 if type "rg" > /dev/null 2>&1; then
-  echo "ripgrep is already installed."
+  echo -e "ripgrep is already $installed."
 else
-  echo "ripgrep not found."
-  sudo apt install -y ripgrep
+  echo -e "ripgrep is $not_found."
+  echo -e "Please install ripgrep."
 fi
 
 # Install nodejs
 if type "node" > /dev/null 2>&1; then
-  echo "nodejs is already installed."
+  echo -e "nodejs is already $installed."
 else
-  echo "nodejs not found."
-  curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-  sudo apt-get install -y nodejs
+  echo -e "nodejs is $not_found."
+  echo -e "Please install nodejs."
 fi
 
 
