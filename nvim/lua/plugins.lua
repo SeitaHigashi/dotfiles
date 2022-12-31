6742f227b5eb060a2a194d30a6534c196bc3d252
SeitaHigashi <higashi110902@gmail.com>
2022-12-31 23:30:35 +0900

    Update the config that will be silent save

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -175,6 +175,7 @@ require('packer').startup(function()
     config = function()
       vim.g.auto_save = 1
       vim.g.auto_save_in_insert_mode = 0
+      vim.g.auto_save_silent = 1
     end,
   }
 
