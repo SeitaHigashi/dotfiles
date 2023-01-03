080a99ebbb671ba889aa0d24aaa09daef8e80592
SeitaHigashi <higashi110902@gmail.com>
2023-01-03 17:20:01 +0900

    Updated the lualine settings.

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -7,6 +7,17 @@ return function ()
     end
   end
 
+  local function clock()
+    if vim.fn.winwidth(0) > 70 then
+      if os.time() % 2 == 0 then
+        return ' ' .. os.date("%H:%M")
+      else
+        return ' ' .. os.date("%H %M")
+      end
+    end
+    return ''
+  end
+
   require'lualine'.setup {
     options = {
       icons_enabled = true,
@@ -18,22 +29,23 @@ return function ()
       globalstatus = true,
     },
     sections = {
-      lualine_a = {'mode'},
-      lualine_b = {readonly, 'branch'},
-      lualine_c = {'filename', 'diff'},
-      lualine_x = {'diagnostics', 'encoding', 'fileformat', 'filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
+      lualine_a = { 'mode' },
+      lualine_b = { readonly, 'branch' },
+      lualine_c = { 'filename', 'diff' },
+      lualine_x = { 'diagnostics', 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location', clock }
     },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {'branch'},
-      lualine_c = {'filename'},
-      lualine_x = {'filetype'},
-      lualine_y = {'location'},
-      lualine_z = {}
-    },
+    --  if laststatus is 3, disabled inactive_sections.
+    --inactive_sections = {
+    --  lualine_a = {},
+    --  lualine_b = {'branch'},
+    --  lualine_c = {'filename'},
+    --  lualine_x = {'filetype'},
+    --  lualine_y = {'location'},
+    --  lualine_z = { clock }
+    --},
     tabline = {},
-    extensions = {}
+    extensions = {'fugitive', 'quickfix'}
   }
 end
