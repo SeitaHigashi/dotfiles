504e82b9f354ce0daad7713a2f28f703f65f94cc
SeitaHigashi <higashi110902@gmail.com>
2023-01-03 16:45:49 +0900

    Updated the telescope settings

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -20,6 +20,8 @@ return function ()
   telescope.setup{
     defaults = {
       layout_strategy = 'vertical',
+      layout_config = { prompt_position = 'top' },
+      sorting_strategy = 'ascending',
       mappings = {
         i = {
           ["J"] = actions.move_selection_next,
