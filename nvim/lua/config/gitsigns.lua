44f53cf9e3dfddb1bdff375d02ee626745f5d9aa
SeitaHigashi <higashi110902@gmail.com>
2023-01-04 18:25:12 +0900

    Updated the gitsigns config

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -8,8 +8,8 @@ return function ()
       changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
     },
     signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-    numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-    linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+    numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl     = true, -- Toggle with `:Gitsigns toggle_linehl`
     word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
     keymaps = {
       -- Default keymap options
@@ -38,11 +38,11 @@ return function ()
       follow_files = true
     },
     attach_to_untracked = true,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
       virt_text = true,
-      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-      delay = 1000,
+      virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+      delay = 500,
       ignore_whitespace = false,
     },
     current_line_blame_formatter_opts = {
@@ -52,16 +52,5 @@ return function ()
     update_debounce = 100,
     status_formatter = nil, -- Use default
     max_file_length = 40000,
-    preview_config = {
-      -- Options passed to nvim_open_win
-      border = 'single',
-      style = 'minimal',
-      relative = 'cursor',
-      row = 0,
-      col = 1
-    },
-    yadm = {
-      enable = false
-    },
   }
 end
