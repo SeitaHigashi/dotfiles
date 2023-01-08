95283e4fd0bb76fac3224454591263383cfe24ab
SeitaHigashi <higashi110902@gmail.com>
2023-01-08 15:58:11 +0900

    feat: applied the new configuration.

--- nvim/lua/config/nvim-notify.lua
+++ nvim/lua/config/nvim-notify.lua
@@ -4,6 +4,10 @@ return function ()
     render = "minimal",
     timeout = 1000,
     stages = "slide",
+    top_down = false,
     max_height = 5,
+    max_width = function ()
+      return vim.fn.winwidth(0) * 0.7
+    end
   })
 end
