387cd089a96f4cd8ef8697932e28e5250bb5de15
Seita Higashi <higashi110902@gmail.com>
2023-09-19 16:25:20 +0000

    feat: add 'Wansmer/symbol-usage.nvim', 'VidocqH/lsp-lens.nvim'

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -119,5 +119,23 @@ return {
   {
     'xiyaowong/transparent.nvim',
     lazy = false,
-  }
+  },
+
+  {
+    'Wansmer/symbol-usage.nvim',
+    event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+    config = function()
+      require('symbol-usage').setup()
+    end
+  },
+
+  {
+    'VidocqH/lsp-lens.nvim',
+    event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+    opts = {},
+  },
 }
+
+
+
+
