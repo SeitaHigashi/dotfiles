fc12d5277e32137dd1f712f4c6d8471f6dc87bff
Seita Higashi <higashi110902@gmail.com>
2023-12-03 05:43:31 +0000

    chore: remove lsp-usage

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -123,14 +123,6 @@ return {
   },
 
   {
-    'Wansmer/symbol-usage.nvim',
-    event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
-    config = function()
-      require('symbol-usage').setup()
-    end
-  },
-
-  {
     'VidocqH/lsp-lens.nvim',
     event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
     opts = {},
