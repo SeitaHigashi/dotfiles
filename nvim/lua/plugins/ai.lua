a6b2f6cc3262f0d7e9c0e74b510c549c19f3ceb7
Seita Higashi <higashi110902@gmail.com>
2024-04-01 23:33:07 +0900

    feat: ollama.nvim

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -128,4 +128,34 @@ return {
       }
     },
   },
+
+  {
+    'nomnivore/ollama.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+    keys = {
+    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+      {
+        "<leader>oo",
+        ":<c-u>lua require('ollama').prompt()<cr>",
+        desc = "ollama prompt",
+        mode = { "n", "v" },
+      },
+
+      -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+      {
+        "<leader>oG",
+        ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+        desc = "ollama Generate Code",
+        mode = { "n", "v" },
+      },
+    },
+    ---@type Ollama.Config
+    opts = {
+      model = "gemma",
+      url = "http://arch-kde.seita.home:11434",
+    }
+  },
 }
