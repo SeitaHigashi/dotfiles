a6c7ac74689f1be0e448223339c461cc5f93b7b1
Seita Higashi <higashi110902@gmail.com>
2024-10-11 00:46:24 +0900

    feat: enabled home-manager as NixOS native module

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,15 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      myNixOS = inputs.nixpkgs.lib.nixosSystem {
+      myNixOS = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-	modules = [
-	  /etc/nixos/configuration.nix
-	];
+        modules = [
+          /etc/nixos/configuration.nix
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.seita = import ../home-manager/home.nix;
+          }
+        ];
       };
     };
   };
