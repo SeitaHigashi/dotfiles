dbd275110101b6d39479c0582d553d01cd6f9e12
Seita Higashi <higashi110902@gmail.com>
2025-01-20 22:09:01 +0900

    feat(nixos): update flake.nix configuration
    add seita-nixos configuration for desktop PC

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,9 +5,24 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs @ { nixpkgs, home-manager, ... }:
+  {
     nixosConfigurations = {
       myNixOS = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          /etc/nixos/configuration.nix
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.seita = import ../home-manager/home.nix;
+          }
+        ];
+      };
+      seita-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           /etc/nixos/configuration.nix
