1b14a57bd4b073af582a50f1a02c856b5be6e47f
Seita Higashi <higashi110902@gmail.com>
2025-09-28 00:55:50 +0900

    fix(nixos): update GNOME and pulseaudio service options
    Switch GNOME and pulseaudio configuration to use new service option
    names. This aligns with recent NixOS module changes, ensuring correct
    desktop and audio setup. The previous options under services.xserver and
    hardware are now deprecated.

--- nixos/seita-mac-nix/configuration.nix
+++ nixos/seita-mac-nix/configuration.nix
@@ -72,8 +72,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   environment.gnome.excludePackages = with pkgs; [
     gnome-tour
@@ -101,7 +101,7 @@
   ];
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
