38b60798254aa9b58f3d728cac2795c32b3e585c
Seita Higashi <higashi110902@gmail.com>
2026-05-10 22:04:45 +0900

    chore: diagnostic sign configurations

--- nvim/lua/lsp-configs.lua
+++ nvim/lua/lsp-configs.lua
@@ -29,7 +29,14 @@ end
 
 vim.diagnostic.config({
   virtual_text = false,
-  signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.HINT] = '',
+      [vim.diagnostic.severity.INFO] = '',
+    },
+  },
   underline = true,
   update_in_insert = false,
   sererity_sort = false,
